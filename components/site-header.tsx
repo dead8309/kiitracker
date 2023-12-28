e1a320e45c94e2c64c1409501841fd38ea1a7af7
Vaibhav <vaibhavmonu8309@gmail.com>
2023-12-28 20:49:52 +0530

    ui: update nav bar

--- components/site-header.tsx
+++ components/site-header.tsx
@@ -1,19 +1,20 @@
-import Link from "next/link"
-
 import { siteConfig } from "@/config/site"
 import { MainNav } from "@/components/main-nav"
-import { ThemeToggle } from "@/components/theme-toggle"
+
 import UserButton from "./user-button"
+import MobileNav from "./mobile-nav"
 
 export function SiteHeader() {
   return (
-    <header className="bg-background sticky top-0 z-40 w-full border-b">
+    <header className="bg-brand/brand-dark sticky top-0 z-40 w-full border-b border-Neutrals/neutrals-11 lg:border-none">
       <div className="container flex h-16 items-center space-x-4 sm:justify-between sm:space-x-0">
         <MainNav items={siteConfig.mainNav} />
-        <div className="flex flex-1 items-center justify-end space-x-4">
-          <nav className="flex items-center space-x-1">
-            <ThemeToggle />
+        <div className="flex flex-1 items-center justify-end">
+          <nav className="flex items-center space-x-2">
             <UserButton />
+            <div className="md:hidden">
+              <MobileNav items={siteConfig.mainNav}/>
+            </div>
           </nav>
         </div>
       </div>
