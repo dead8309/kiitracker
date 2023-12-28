d6904c7ee532d7a8fb03ae3352f907d45006fe0e
Vaibhav <vaibhavmonu8309@gmail.com>
2023-12-28 20:48:01 +0530

    ui: configure configs acc to figma design
    - change fonts to dm sans
    - add nav and footer links
    - add image assets
    - change some css styles

--- config/site.ts
+++ config/site.ts
@@ -4,9 +4,75 @@ export const siteConfig = {
   name: "KIITracker",
   description:
     "A simple route tracker for KIIT University students to track their routine.",
-  mainNav: [],
+  mainNav: [
+    {
+      title: "Features",
+      href: "/#features",
+    },
+    {
+      title: "Process",
+      href: "/#our-process",
+    },
+    {
+      title: "Faq",
+      href: "/#faq",
+    },
+    {
+      title: "Download",
+      href: "/#download",
+    },
+  ],
   youtube: "https://www.youtube.com/channel/UCh-zsCv66gwHCIbMKLMJmaw",
   github: "https://github.com/dead8309",
   privacyPolicy: "/",
   termsAndConditions: "/",
+  footerLinks: [
+    {
+      title: "About us",
+      links: [
+        {
+          name: "Support",
+          href: "#",
+        },
+        {
+          name: "Features",
+          href: "#",
+        },
+        {
+          name: "Terms",
+          href: "#",
+        },
+      ],
+    },
+    {
+      title: "Useful Links",
+      links: [
+        {
+          name: "Privacy Policy",
+          href: "#",
+        },
+        {
+          name: "Documentation",
+          href: "#",
+        },
+        {
+          name: "GitHub",
+          href: "#",
+        },
+      ],
+    },
+    {
+      title: "Contact",
+      links: [
+        {
+          name: "Developer",
+          href: "#",
+        },
+        {
+          name: "Faq",
+          href: "#",
+        },
+      ],
+    },
+  ],
 }
--- lib/fonts.ts
+++ lib/fonts.ts
@@ -1,11 +1,7 @@
-import { JetBrains_Mono as FontMono, Inter as FontSans } from "next/font/google"
+import { DM_Sans as FontDm } from "next/font/google"
 
-export const fontSans = FontSans({
-  subsets: ["latin"],
-  variable: "--font-sans",
-})
-
-export const fontMono = FontMono({
-  subsets: ["latin"],
-  variable: "--font-mono",
+export const fontDmSans = FontDm({
+  subsets: ['latin'],
+  weight: ['500', "600", "700"],
+  variable: '--font-dm'
 })
