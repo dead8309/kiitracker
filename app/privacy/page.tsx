e420936050e51bb6ee5e65e6372682d3b4767eff
Vaibhav <vaibhavmonu8309@gmail.com>
2024-01-02 22:37:27 +0530

    add /privacy and /terms page

---
+++ app/privacy/page.tsx
@@ -0,0 +1,33 @@
+import React from "react"
+
+const PrivacyPage = () => {
+  return (
+    <div className="container flex flex-col items-center mt-10">
+      <h1 className="text-3xl sm:text-6xl font-bold">Privacy Policy</h1>
+      <div className="mt-4 w-full sm:w-2/3">
+        <p className="text-lg">
+          In the course of using the KIITracker application ("KIITracker"), we
+          collect certain personal information to provide you with our services.
+          This information includes your name, email, and class schedule. It is
+          important to clarify that we do not sell or share this information
+          with third parties. Additionally, we may collect non-personal
+          information, such as device details, IP addresses, and usage patterns,
+          to enhance our services and improve user experience. While we
+          implement security measures to protect your personal information from
+          unauthorized access or disclosure, it's essential to recognize that no
+          method of transmission over the internet or electronic storage is
+          entirely secure. The App may contain links to third-party websites or
+          services. Please be aware that we do not control or take
+          responsibility for the privacy practices of these third parties. We
+          reserve the right to update our Privacy Policy at any time, and any
+          changes will be communicated by posting the new Privacy Policy on this
+          page. It is recommended to review this policy periodically for
+          updates. If you have any questions or concerns about our Privacy
+          Policy, please contact us at <strong>deadyt8309@gmail.com</strong>.
+        </p>
+      </div>
+    </div>
+  )
+}
+
+export default PrivacyPage
---
+++ app/terms/page.tsx
@@ -0,0 +1,28 @@
+import React from "react"
+
+const TermsPage = () => {
+  return (
+    <div className="container flex flex-col items-center mt-10">
+      <h1 className="text-3xl sm:text-6xl font-bold">Terms of Service</h1>
+      <div className="mt-4 w-full sm:w-2/3">
+        <p className="text-lg">
+          By utilizing the KIITracker application ("KIITracker"), you hereby
+          acknowledge and agree to comply with the Terms of Service outlined
+          herein. In the event that you disagree with any aspect of these terms,
+          we kindly request that you refrain from using the App. The use of the
+          KIITracker App is permitted solely for personal, non-commercial
+          purposes, and any unauthorized usage is strictly prohibited. We retain
+          the right to modify, suspend, or discontinue the App, or any of its
+          components, at our discretion and without prior notice. It is
+          important to note that we bear no responsibility for any indirect,
+          incidental, special, consequential, or punitive damages, nor any loss
+          of profits or revenues arising from the use of our services. These
+          terms are subject to change, and it is advised to review them
+          periodically for any updates or modifications.
+        </p>
+      </div>
+    </div>
+  )
+}
+
+export default TermsPage
