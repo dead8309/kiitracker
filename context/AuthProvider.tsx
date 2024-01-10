b5aeea64b2e003a2da076fb626af89406c615166
Vaibhav <vaibhavmonu8309@gmail.com>
2024-01-10 17:16:29 +0530

    dont show SecurelyLogginIn

--- context/AuthProvider.tsx
+++ context/AuthProvider.tsx
@@ -59,7 +59,8 @@ export const AuthContextProvider = ({
       }}
     >
       {loading ? (
-        <SecurelyLogginIn />
+        // <SecurelyLogginIn />
+        null
       ) : (
         children
       )}
